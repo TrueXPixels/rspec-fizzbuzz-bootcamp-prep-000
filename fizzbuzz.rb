bcb26f79371d9ebb45bdd12ffe5ce089290cb9cf
Loren Cerri <plexidev@gmail.com>
2018-06-21 18:02:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(num) 
+def fizzbuzz(num = 0) 
   if (num % 3 == 0) && (num % 5 == 0)
     "FizzBuzz"
   else if num % 3 == 0
